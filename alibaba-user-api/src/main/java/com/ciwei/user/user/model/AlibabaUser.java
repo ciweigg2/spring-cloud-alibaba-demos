e4438419ab5a68f8e077da0e6ae8a31cfd225044
ciweigg2 <1085160569@qq.com>
2019-08-28 21:55:05 +0800

    update

--- alibaba-user-api/src/main/java/com/ciwei/user/user/model/AlibabaUser.java
+++ alibaba-user-api/src/main/java/com/ciwei/user/user/model/AlibabaUser.java
@@ -4,18 +4,16 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
 
-/** 
+import java.io.Serializable;
+import java.util.Date;
+
+/**
  * @NAME AlibabaUser
  * @USER Ciwei
  * @DATE 2019/8/26/026 12:30
 **/
-@ApiModel(value="com.ciwei.user.user.model.AlibabaUser")
 @Data
 @TableName(value = "alibaba_user")
 public class AlibabaUser implements Serializable {
@@ -23,21 +21,18 @@ public class AlibabaUser implements Serializable {
      * 用户id
      */
     @TableId(value = "user_id", type = IdType.INPUT)
-    @ApiModelProperty(value="用户id")
     private Long userId;
 
     /**
      * 用户名
      */
     @TableField(value = "user_name")
-    @ApiModelProperty(value="用户名")
     private String userName;
 
     /**
      * 创建时间
      */
     @TableField(value = "create_time")
-    @ApiModelProperty(value="创建时间")
     private Date createTime;
 
     private static final long serialVersionUID = 1L;
