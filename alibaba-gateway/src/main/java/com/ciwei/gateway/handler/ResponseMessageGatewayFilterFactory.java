ff4971a61f308316669d438e05aca18156312e07
maxiucheng <maxiucheng@example.com>
2019-09-03 10:43:57 +0800

    :wrench: 修改返回数据

--- alibaba-gateway/src/main/java/com/ciwei/gateway/handler/ResponseMessageGatewayFilterFactory.java
+++ alibaba-gateway/src/main/java/com/ciwei/gateway/handler/ResponseMessageGatewayFilterFactory.java
@@ -26,42 +26,46 @@ import java.util.Map;
 @Component
 public class ResponseMessageGatewayFilterFactory extends ModifyResponseBodyGatewayFilterFactory {
 
-	@Autowired
-	private ResponseMessageMapper responseMessageMapper;
+    @Autowired
+    private ResponseMessageMapper responseMessageMapper;
 
-	@Override
-	public GatewayFilter apply(Config config) {
-		return new ModifyResponseGatewayFilter(this.getConfig());
-	}
+    @Override
+    public GatewayFilter apply(Config config) {
+        return new ModifyResponseGatewayFilter(this.getConfig());
+    }
 
-	private Config getConfig() {
-		Config cf = new Config();
-		// Config.setRewriteFunction(Class<T> inClass, Class<R> outClass, RewriteFunction<T, R> rewriteFunction)
-		// inClass 原数据类型，可以指定为具体数据类型，我这里指定为Object,是为了处理多种数据类型。
-		// 当然支持多接口返回多数据类型的统一修改，yaml中的配置，path,uri需要做相关调整
-		// outClass 目标数据类型
-		// rewriteFunction 内容重写方法
-		cf.setRewriteFunction(Object.class, ResponseMessage.class, getRewriteFunction());
-		return cf;
-	}
+    private Config getConfig() {
+        Config cf = new Config();
+        // Config.setRewriteFunction(Class<T> inClass, Class<R> outClass, RewriteFunction<T, R> rewriteFunction)
+        // inClass 原数据类型，可以指定为具体数据类型，我这里指定为Object,是为了处理多种数据类型。
+        // 当然支持多接口返回多数据类型的统一修改，yaml中的配置，path,uri需要做相关调整
+        // outClass 目标数据类型
+        // rewriteFunction 内容重写方法
+        cf.setRewriteFunction(Object.class, ResponseMessage.class, getRewriteFunction());
+        return cf;
+    }
 
-	private RewriteFunction<Object, ResponseMessage> getRewriteFunction() {
-		return (exchange, resp) -> {
-			ResponseEntity responseEntity = new ResponseEntity();
-			try {
-				//将返回的map转成对象
-				BeanUtils.populate(responseEntity, (Map<String, ? extends Object>) resp);
-			} catch (Exception e) {
-				//如果类型转换错误设置默认值
-				responseEntity.setStatus(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
-				responseEntity.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-				responseEntity.setMessage(e.getMessage());
-			}
-			return Mono.just(
-					//使用mapstruct转换dto返回
-					responseMessageMapper.responseEntityToResponseMessage(responseEntity)
-			);
-		};
-	}
+    private RewriteFunction<Object, ResponseMessage> getRewriteFunction() {
+        return (exchange, resp) -> {
+            ResponseEntity responseEntity = new ResponseEntity();
+            try {
+                //将返回的map转成对象
+                BeanUtils.populate(responseEntity, (Map<String, ? extends Object>) resp);
+            } catch (Exception e) {
+                //如果类型转换错误设置默认值
+                responseEntity.setStatus(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+                responseEntity.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+                responseEntity.setMessage(e.getMessage());
+            }
+            //使用mapstruct转换dto返回
+            ResponseMessage responseMessage = responseMessageMapper.responseEntityToResponseMessage(responseEntity);
+            if (responseMessage.getData() == null) {
+                responseMessage.setMessage("failed");
+                responseMessage.setData(responseEntity.getMessage());
+            }
+            return Mono.just(responseMessage);
+        };
+
+    }
 
 }
