4bf0616411198be2eec4f22f0eb4d4f5be927bbc
maxiucheng <maxiucheng@example.com>
2019-09-10 17:00:18 +0800

    :white_check_mark: update

--- alibaba-starter/alibaba-ribbon-spring-boot-starter/src/main/java/com/ciwei/ribbon/config/RestTemplateProperties.java
+++ alibaba-starter/alibaba-ribbon-spring-boot-starter/src/main/java/com/ciwei/ribbon/config/RestTemplateProperties.java
@@ -12,22 +12,27 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @Setter
 @Getter
-@ConfigurationProperties(prefix = "zlt.rest-template")
+@ConfigurationProperties(prefix = "alibaba.rest-template")
 public class RestTemplateProperties {
+
     /**
      * 最大链接数
      */
     private int maxTotal = 200;
+
     /**
      * 同路由最大并发数
      */
     private int maxPerRoute = 50;
+
     /**
      * 读取超时时间 ms
      */
     private int readTimeout = 35000;
+
     /**
      * 链接超时时间 ms
      */
     private int connectTimeout = 10000;
+
 }
