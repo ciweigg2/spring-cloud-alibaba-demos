8b48686f49150ed8366a10e64caae94eb412c7f1
maxiucheng <maxiucheng@example.com>
2019-09-17 16:00:39 +0800

    :lipstick: 修改开发规范

--- alibaba-gift/src/main/java/com/ciwei/gift/AlibabaGiftSpringBootApplication.java
+++ alibaba-gift/src/main/java/com/ciwei/gift/AlibabaGiftSpringBootApplication.java
@@ -1,7 +1,6 @@
 package com.ciwei.gift;
 
 import com.ciwei.ribbon.annotation.EnableServerVersionRule;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
