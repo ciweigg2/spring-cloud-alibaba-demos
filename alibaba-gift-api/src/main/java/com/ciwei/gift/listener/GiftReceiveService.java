788e04b7eb03b6e95e550060d4043d45718f4636
maxiucheng <maxiucheng@example.com>
2019-09-26 17:59:35 +0800

    解决分布式事务最终一致性的问题

--- alibaba-gift-api/src/main/java/com/ciwei/gift/listener/GiftReceiveService.java
+++ alibaba-gift-api/src/main/java/com/ciwei/gift/listener/GiftReceiveService.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 礼物服务的消费者，接收到添加用户成功后增加礼物
